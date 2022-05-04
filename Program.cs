c8c8997382a91c6a669e49af8a1a358d9c1a06da
Gilbert Gonzales <gigonz@extendhealth.com>
2022-05-04 16:28:23 -0600

    Initial outline

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StarTrekvsStarWars
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the name of the Star Trek ship.");
+            var StarTrekShipName = Console.ReadLine();
+            System.Console.WriteLine("Please Enter the name of the Star Wars ship.");
+            var StarWarsShipName = Console.ReadLine();
+            System.Console.WriteLine($"This Ship {StarTrekShipName} is better than {StarWarsShipName}");
+            System.Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+
+    }
+
+}
+
