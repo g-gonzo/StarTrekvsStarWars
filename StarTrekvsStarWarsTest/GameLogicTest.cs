7011ff6240fbf0c6e4ad8ba6fb35188d12da9ec4
Jonathan Stutson <jonathan.stutson@extendhealth.com>
2022-07-19 10:53:47 -0600

    Fixed failing unit test and reoganized files

--- StarTrekvsStarWarsTest/GameLogicTest.cs
+++ StarTrekvsStarWarsTest/GameLogicTest.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StarTrekvsStarWars;
 using System.IO;
 using System;
+using System.Text;
 
 namespace StarTrekvsStarWarsTest
 {
@@ -14,7 +15,6 @@ namespace StarTrekvsStarWarsTest
             GameLogic gl = new GameLogic();
             var stringWriter = new StringWriter();
             Console.SetOut(stringWriter);
-
             var stringReader = new StringReader("n");
             Console.SetIn(stringReader);
 
@@ -32,7 +32,6 @@ namespace StarTrekvsStarWarsTest
             GameLogic gl = new GameLogic();
             var stringWriter = new StringWriter();
             Console.SetOut(stringWriter);
-
             var stringReader = new StringReader("N");
             Console.SetIn(stringReader);
 
@@ -50,19 +49,16 @@ namespace StarTrekvsStarWarsTest
             GameLogic gl = new GameLogic();
             var stringWriter = new StringWriter();
             Console.SetOut(stringWriter);
-            var stringFirstReader = new StringReader("No");
-            Console.SetIn(stringFirstReader);
-            var stringSecondReader = new StringReader("y");
-            Console.SetIn(stringSecondReader);
-            
-
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("RandomText");
+            stringBuilder.AppendLine("n");
+            var stringReader = new StringReader(stringBuilder.ToString());
+            Console.SetIn(stringReader);
 
             gl.PlayGame();
 
-            var playOutput = stringWriter.ToString();
-           // var correctionOutput = stringWriterCorrectionQuestion.ToString();
-            //Assert.AreEqual("Would you like to play a game? (Y)es or (N)o\r\n", playOutput);
-            Assert.AreEqual("Please enter ONLY Y or N. Would you like to play a game?\r\n", playOutput);
+            var output = stringWriter.ToString();
+            Assert.AreEqual("Would you like to play a game? (Y)es or (N)o\r\nPlease enter ONLY Y or N. Would you like to play a game?\r\n", output);
             Assert.IsFalse(gl.isGameInProgress);
             Assert.IsFalse(gl.needToAskUserToPlay);
         }
